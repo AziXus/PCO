0dbfa5e2334cd9baf288939fc4896b68fc85b03c
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-03-13 08:46:01 +0100

    Labo3

--- labo3/code/src/mythread.h
+++ labo3/code/src/mythread.h
@@ -1,5 +1,10 @@
 #ifndef MYTHREAD_H
 #define MYTHREAD_H
+#include <QString>
+#include "threadmanager.h"
 
+void init(const QString& charset, const QString& salt, const QString& hash, unsigned int nbChars);
+void crack(QVector<unsigned int> currentPasswordArray, long long unsigned int nbToCompute, double increment, ThreadManager* tm);
+QString getPassword();
 
 #endif // MYTHREAD_H
